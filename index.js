0d4ec72ead6c6733ffc7b306583f47eeab2610f4
Emre Tapcı <emre@ersteyazilim.com>
2018-10-08 16:01:06 +0300

    in decode() func, typeof Buffer check removed.

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ module.exports = {
 			: encoded.reverse().concat(new Array(k - encoded.length).fill(0));
 	},
 	decode: function (arr, k, bigEndian) {
-		if (typeof arr === 'Buffer')
-			arr = [...arr];
+		arr = [...arr];
 		let res = parseInt('0x' + (bigEndian ? arr.concat(new Array(k - arr.length).fill(0)) : new Array(k - arr.length).fill(0).concat(arr.reverse())).
 			reduce((total, next) => total += next.toString(16).padStart(2, '0'), ''), 16);
 		if (res >= parseInt('0x80' + '00'.repeat(k - 1)))
